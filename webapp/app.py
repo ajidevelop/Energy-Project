aee8a075b264baca03c9cb81b4b8a72bade511ac
SafeDev <ajidevelop@gmail.com>
2018-07-20 10:57:53 -0500

    started on styling

--- webapp/app.py
+++ webapp/app.py
@@ -25,7 +25,7 @@ def index():
     else:
         print(lo.user_logged_in)
         pass  # set logged in functionality
-        return render_template('web/entry_page.html')
+        return render_template('entry_page.html')
 
 
 @app.route("/<string:rero>")
@@ -34,12 +34,12 @@ def reroute(rero):
         return redirect(url_for('login'))
     else:
         pass  # set logged in functionality
-        return render_template('web/entry_page.html')
+        return render_template('entry_page.html')
 
 
 @app.route('/logout', methods=['POST'])
 def logout():
-    return render_template('web/index.html', text='Logged Out')
+    return render_template('index.html', text='Logged Out')
 
 
 @app.route("/login", methods=['POST', 'GET'])
@@ -49,18 +49,18 @@ def login():
         password = request.form.get('password')
         log = check_user(username, password)
         print(log)
-        return render_template('web/index.html', text=log, loggedin=True)
+        return render_template('index.html', text=log, loggedin=True)
     except e.WrongPassword:
-        return render_template('web/index.html', text=w_credentials, showlogin=True)
+        return render_template('index.html', text=w_credentials, showlogin=True)
     except e.NeedVerificationCode:
-        return render_template('web/index.html', verification=True, showlogin=True)
+        return render_template('index.html', verification=True, showlogin=True)
     except TypeError:
-        return render_template('web/index.html')
+        return render_template('index.html')
 
 
 @app.route('/new_user', methods=['POST', 'GET'])
 def new_user():
-    return render_template('web/new_user.html')
+    return render_template('new_user.html')
 
 
 @app.route('/verify', methods=['POST', 'GET'])
@@ -69,14 +69,14 @@ def verify():
     if dcV.check_verification_token(token):
         user = dcU.email_verified(False, token)
         lo.user_logged_in = True
-        return render_template('web/index.html', text=f'Welcome {user}', loggedin=True)
+        return render_template('index.html', text=f'Welcome {user}', loggedin=True)
     else:
-        return render_template('web/index.html', text='Incorrect Verification Code')
+        return render_template('index.html', text='Incorrect Verification Code')
 
 
 @app.route('/verify/resend_request', methods=['POST', 'GET'])
 def resend_request():
-    return render_template('web/email.html')
+    return render_template('email.html')
 
 
 @app.route('/verify/resend', methods=['POST'])
@@ -84,9 +84,9 @@ def resend():
     email = request.form.get('email')
     try:
         dcV.resend_verification(email)
-        return render_template('web/index.html', text='Email sent')
+        return render_template('index.html', text='Email sent')
     except e.NoEmail:
-        return render_template('web/email.html', text='Email not in system')
+        return render_template('email.html', text='Email not in system')
 
 
 @app.route('/create', methods=['POST'])
@@ -98,13 +98,13 @@ def create_user():
         cu(username, password, email)
     except e.TakenField as error:
         if str(error) == f'The username "{username}" is already taken':
-            return render_template('web/new_user.html', text=error)
+            return render_template('new_user.html', text=error)
         elif str(error) == f'The email "{email}" is already taken':
-            return render_template('web/new_user.html', text=error)
+            return render_template('new_user.html', text=error)
     except e.InvalidEmail:
-        return render_template('web/new_user.html', text='Please enter a valid email')
+        return render_template('new_user.html', text='Please enter a valid email')
     else:
-        return render_template('web/index.html', text='Verification Code sent to your email')
+        return render_template('index.html', text='Verification Code sent to your email')
 
 
 if __name__ == '__main__':
