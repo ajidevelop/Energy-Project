a730a9c8fecb5b8f078442b1fc756f53297927de
SafeDev <ajidevelop@gmail.com>
2018-07-23 23:59:41 -0500

    create_user has interface

--- webapp/static/js/login.js
+++ webapp/static/js/login.js
@@ -1,5 +1,8 @@
-var modal = document.getElementById('modal');
-var modal_content = document.getElementById('modal-content');
+let modal = document.getElementById('modal');
+let modal_content = document.getElementById('modal-content');
+let logon = document.getElementById('logon');
+let create_account = document.getElementById('create-account');
+let img = document.getElementById('sample-avatar');
 
 window.onclick = function (event) {
     if (event.target === modal) {
@@ -17,6 +20,14 @@ function reload() {
 }
 
 function new_user() {
+    logon.style.display='none';
+    create_account.style.display='block';
+    img.style.display='none'
+}
 
+function go_back() {
+    logon.style.display='block';
+    create_account.style.display='none';
+    img.style.display='block'
 }
 
