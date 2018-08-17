5fae489860b33d15af663d5ecaf34579b799d6c7
SafeDev <ajidevelop@gmail.com>
2018-08-17 16:43:07 -0500

    openmarkdown works

--- Markdowns/openmarkdown.py
+++ Markdowns/openmarkdown.py
@@ -1,15 +1,30 @@
 __author__ = 'DanielAjisafe'
 
-import os, sys, subprocess
-
-def open_file(filename):
-    if sys.platform == 'win32':
-        os.startfile(filename)
-    opener = 'open' if sys.platform == 'darwin' else 'xdg-open'
-    subprocess.call([opener, filename])
-
+import os
+import sys
+import subprocess
 
 challenges = '/Users/danielajisafe/Energy-Project/Markdowns/challenges.md' if sys.platform == 'darwin' else ''
 bugs = '/Users/danielajisafe/Energy-Project/Markdowns/bugs.md' if sys.platform == 'darwin' else ''
 
-open_file(challenges)
+
+def open_file(filename):
+    if sys.platform == 'win32':
+        os.startfile(filename)
+
+    opener = 'open' if sys.platform == 'darwin' else 'xdg-open'
+    subprocess.call([opener, filename])
+
+
+def open_bugs(filename=''):
+    bug = open(bugs)
+    for num, line in enumerate(bug, 1):
+        if '### Bugs to fix' in line:
+            lineNumber = num + 1
+    if sys.platform == 'win32':
+        os.startfile(filename)
+    elif sys.platform == 'darwin':
+        os.system(f'code -g {bugs}:{lineNumber}')
+
+
+open_bugs()
--- python_console/test.py
+++ python_console/test.py
@@ -1,13 +1,3 @@
-import os, sys, subprocess
+p = 1
 
-def open_file(filename):
-    if sys.platform == 'win32':
-        os.startfile(filename)
-    opener = 'open' if sys.platform == 'darwin' else 'xdg-open'
-    subprocess.call([opener, filename])
-
-
-challenges = '/Users/danielajisafe/Energy-Project/Markdowns/challenges.md' if sys.platform == 'darwin' else ''
-bugs = '/Users/danielajisafe/Energy-Project/Markdowns/bugs.md' if sys.platform == 'darwin' else ''
-
-open_file(challenges)
+print(f'{p}')
